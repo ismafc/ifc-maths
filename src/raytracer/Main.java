9cf563877c4931f1bdda4531b8569ede8699cbb4
ismael.flores <ismael.flores@Ismael.CONTROLNET>
2013-05-25 11:05:38 +0200

    Añadimos funciones para calcular los factores primos y la factorización de un número natural

--- src/raytracer/Main.java
+++ src/raytracer/Main.java
@@ -89,6 +89,29 @@ Números de Friedman:
         return true;
     }
 
+    public static ArrayList<Long> primeFactors(long n) {
+        ArrayList<Long> factors = new ArrayList<Long>();
+        for (long i = 2; i <= n; i++) {
+          while (n % i == 0) {
+            factors.add(i);
+            n /= i;
+          }
+        }
+        return factors;
+    }
+
+    public static HashMap<Long, Long> primeFactorization(long n) {
+        HashMap<Long, Long> factors = new HashMap<Long, Long>();
+        for (long i = 2; i <= n; i++) {
+          while (n % i == 0) {
+            // Sumamos 1 al número de capas con ese número de cubos
+            factors.put(i, !factors.containsKey(i) ? 1 : factors.get(i) + 1);
+            n /= i;
+          }
+        }
+        return factors;
+    }
+    
     private static long MCD(long n1, long n2) {
         long lAux = 0;
         if (n1 < n2) {
@@ -1158,141 +1181,169 @@ Números de Friedman:
         System.out.println(probabilidades + " Sobre " + f + " = " + premio + " (" + (1.0 / premio) + ")");
     }
 
+    static public boolean isIn(Point3D c, Point3D p) {
+        if (p.x < 0 || p.y < 0 || p.z < 0)
+            return false;
+        if (p.x >= c.x || p.y >= c.y || p.z >= c.z)
+            return false;
+        return true;
+    }
+    
+    // Start PE 126
+    // Capas de cuboides 
+    // Para cada coordenada (x,y,depth) intentamos cubrir los 6 lados: 
+    // (x+1,y,depth), (x-1,y,depth), (x,y+1,depth), (x,y-1,depth), (x,y,depth+1), (x,y,depth-1)
+    // Descartando los que ya estén
+    static public void ejercicio126() {
+        //C tiene cuantas capas (value) tienen x cubos (key) -> C(22)=2; C(46)=4;
+        HashMap<Integer, Integer> C = new HashMap<>();
+
+        // Máximo número de cubos en la capa 
+        int MAX_CUBES_IN_LAYER = 20000;
+        int FACE_FACTOR = 1;
+        
+        // Almacenamos para cada cuboide (x,y,depth) un hash con todos los cubos calculados hasta el momento
+        HashMap<Point3D, HashMap<Point3D, Boolean>> todas_las_capas = new HashMap<>();
+
+        // Almacenamos para cada cuboide (x,y,depth) un hash con todos los cubos de la última capa
+        HashMap<Point3D, HashMap<Point3D, Boolean>> ultimas_capas = new HashMap<>();
+        
+        int width = 1;
+        while (width <= MAX_CUBES_IN_LAYER / FACE_FACTOR) {
+            for (int height = 1; height <= width; height++) {
+                if ((width * height * 2 + width * 1 * 2 + height * 1 * 2) > MAX_CUBES_IN_LAYER)
+                    break;
+                for (int depth = 1; depth <= height; depth++) {
+                    // Cuboide inicial de width.height.depth
+
+                    int lastCubos = width * height * 2 + width * depth * 2 + height * depth * 2;
+                    int lastDiff = 0;
+                    if (lastCubos > MAX_CUBES_IN_LAYER)
+                        break;
+
+                    // Puntos del cuboide original (width, height, depth) 
+                    // si es la primera capa hay que añadir
+                    // los puntos del cuboide original
+                    Point3D cuboide = new Point3D(width, height, depth);
+                    if (!todas_las_capas.containsKey(cuboide)) {
+                        HashMap<Point3D, Boolean> dots = new HashMap<>();
+                        for (int xx = 0; xx < width; xx++) {
+                            for (int yy = 0; yy < height; yy++) {
+                                for (int zz = 0; zz < depth; zz++) {
+                                    if (xx == 0) {
+                                        Point3D p = new Point3D(xx - 1, yy, zz);
+                                        dots.put(p, Boolean.TRUE);
+                                    }
+                                    if (yy == 0) {
+                                        Point3D p = new Point3D(xx, yy - 1, zz);
+                                        dots.put(p, Boolean.TRUE);
+                                    }
+                                    if (zz == 0) {
+                                        Point3D p = new Point3D(xx, yy, zz - 1);
+                                        dots.put(p, Boolean.TRUE);
+                                    }
+                                    if (xx == width - 1) {
+                                        Point3D p = new Point3D(width, yy, zz);
+                                        dots.put(p, Boolean.TRUE);
+                                    }
+                                    if (yy == height - 1) {
+                                        Point3D p = new Point3D(xx, height, zz);
+                                        dots.put(p, Boolean.TRUE);
+                                    }
+                                    if (zz == depth - 1) {
+                                        Point3D p = new Point3D(xx, yy, depth);
+                                        dots.put(p, Boolean.TRUE);
+                                    }
+                                }
+                            }
+                        }
+                        todas_las_capas.put(cuboide, dots);
+                        ultimas_capas.put(cuboide, dots);
+                    }
+
+                    // Sumamos 1 al número de capas con ese número de cubos
+                    C.put(lastCubos, !C.containsKey(lastCubos) ? 1 : C.get(lastCubos) + 1);
+                    System.out.println("(" + width + "," + height + "," + depth + ") = " + lastCubos + " (" + lastDiff + ")");
+                    // Mientras la última capa del cuboide tenga más de 'MAX_CUBES_IN_LAYER' 
+                    // cubos, seguimos calculando capas
+                    while (lastCubos < MAX_CUBES_IN_LAYER) {
+                    
+                        // Calculamos la nueva capa para el cuboide base (width, height, depth)
+                        // Los cubos de la nueva capa van en 'dotsc'
+                        // En 'dots' tenemos los cubos totales para el cuboide base (width, height, depth)
+                        HashMap<Point3D, Boolean> ultima = (HashMap<Point3D, Boolean>)ultimas_capas.get(cuboide).clone();
+                        HashMap<Point3D, Boolean> dotsc = new HashMap<>();
+                        ultimas_capas.put(cuboide, dotsc);
+                        HashMap<Point3D, Boolean> dots = todas_las_capas.get(cuboide);
+                        Point3D pp;
+
+                        for (Point3D p : ultima.keySet()) {
+                            pp = new Point3D(p.x - 1, p.y, p.z);
+                            if (!isIn(cuboide, pp) && !dots.containsKey(pp) && !dotsc.containsKey(pp)) {
+                                dotsc.put(pp, Boolean.TRUE);
+                            }
+                            pp = new Point3D(p.x + 1, p.y, p.z);
+                            if (!isIn(cuboide, pp) && !dots.containsKey(pp) && !dotsc.containsKey(pp)) {
+                                dotsc.put(pp, Boolean.TRUE);
+                            }
+                            pp = new Point3D(p.x, p.y - 1, p.z);
+                            if (!isIn(cuboide, pp) && !dots.containsKey(pp) && !dotsc.containsKey(pp)) {
+                                dotsc.put(pp, Boolean.TRUE);
+                            }
+                            pp = new Point3D(p.x, p.y + 1, p.z);
+                            if (!isIn(cuboide, pp) && !dots.containsKey(pp) && !dotsc.containsKey(pp)) {
+                                dotsc.put(pp, Boolean.TRUE);
+                            }
+                            pp = new Point3D(p.x, p.y, p.z - 1);
+                            if (!isIn(cuboide, pp) && !dots.containsKey(pp) && !dotsc.containsKey(pp)) {
+                                dotsc.put(pp, Boolean.TRUE);
+                            }
+                            pp = new Point3D(p.x, p.y, p.z + 1);                            
+                            if (!isIn(cuboide, pp) && !dots.containsKey(pp) && !dotsc.containsKey(pp)) {
+                                dotsc.put(pp, Boolean.TRUE);
+                            }
+                        }
+                        int cubos = dotsc.size();
+                        int diff = cubos - lastCubos;
+                        dots.clear();
+                        dots.putAll(ultima);
+                        dots.putAll(dotsc);
+
+                        // Sumamos 1 al número de capas con ese número de cubos
+                        C.put(cubos, !C.containsKey(cubos) ? 1 : C.get(cubos) + 1);
+                        System.out.println("(" + width + "," + height + "," + depth + ") = " + cubos + " (" + diff + ")");
+                        lastCubos = cubos;
+                        if (diff - lastDiff == 8) {
+                            lastDiff = diff;
+                            break;
+                        }
+                        lastDiff = diff;
+                    }
+                    // Calculamos con la fórmula
+                    while (lastCubos <= MAX_CUBES_IN_LAYER) {
+                        lastCubos = lastCubos + lastDiff + 8;
+                        // Sumamos 1 al número de capas con ese número de cubos
+                        C.put(lastCubos, !C.containsKey(lastCubos) ? 1 : C.get(lastCubos) + 1);
+                        lastDiff = lastDiff + 8;
+                        System.out.println("(" + width + "," + height + "," + depth + ") = " + lastCubos + " (" + lastDiff + ")");
+                    }
+                    
+                    todas_las_capas.remove(cuboide);
+                    ultimas_capas.remove(cuboide);
+                }
+            }
+            width++;
+        }
+        for (Integer n : C.keySet()) {
+            System.out.println("C(" + n + ")=" + C.get(n));
+        }
+}    
     /**
      * Entry point funcion
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Start PE 126
-        // Capas de cuboides 
-        // Para cada coordenada (x,y,z) intentamos cubrir los 6 lados: 
-        // (x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1), (x,y,z-1)
-        // Descartando los que ya estén
-        
-        //C tiene cuantas capas (value) tienen x cubos (key) -> C(22)=2; C(46)=4;
-        HashMap<Integer, Integer> C = new HashMap<>();
-
-        // Calcularemos cuboides hasta MAX_CUBOIDE_SIDE x MAX_CUBOIDE_SIDE x MAX_CUBOIDE_SIDE 
-        int MAX_CUBOIDE_SIDE = 20;
-
-        // Si el número de cubos sobrepasa los MAX_CUBES cubos no seguimos
-        int MAX_CUBES = 2000;
-        
-        // Almacenamos para cada cuboide (x,y,z) un hash con todos los cubos calculados hasta el momento
-        HashMap<Point3D, HashMap<Point3D, Boolean>> capas_externas = new HashMap<>();
-        
-        // Calculamos las MAX_CAPAS primeras capas de los cuboides
-        int MAX_CAPAS = 20;
-        int capa = 1;
-        while (capa <= MAX_CAPAS) {
-            
-            for (int w = 1; w <= MAX_CUBOIDE_SIDE; w++) {
-                for (int h = 1; h <= w; h++) {
-                    for (int z = 1; z <= h; z++) {
-                        // Cuboide inicial de w.h.z
-
-                        // Puntos del cuboide original (w,h,z) 
-                        // si es la primera capa hay que añadir
-                        // los puntos del cuboide original
-                        Point3D cuboide = new Point3D(w, h, z);
-                        if (!capas_externas.containsKey(cuboide)) {
-                            HashMap<Point3D, Boolean> dots = new HashMap<>();
-                            for (int xx = 0; xx < w; xx++) {
-                                for (int yy = 0; yy < h; yy++) {
-                                    for (int zz = 0; zz < z; zz++) {
-                                        Point3D p = new Point3D(xx, yy, zz);
-                                        dots.put(p, Boolean.TRUE);
-                                    }
-                                }
-                            }
-                            capas_externas.put(cuboide, dots);
-                        }
-
-                        // Calculamos la nueva capa 'capa' para el cuboide base (w,h,z)
-                        // Los cubos de la nueva capa van en 'dotsc'
-                        // En 'dots' tenemos los cubos totales para el cuboide base (w,h,z)
-                        HashMap<Point3D, Boolean> dotsc = new HashMap<>();
-                        HashMap<Point3D, Boolean> dots = capas_externas.get(cuboide);
-                        Point3D pp;
-                        
-                        if (dots.size() > MAX_CUBES)
-                            continue;
-                        
-                        for (Point3D p : dots.keySet()) {
-                            pp = new Point3D(p.x - 1, p.y, p.z);
-                            if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                dotsc.put(pp, Boolean.TRUE);
-                            }
-                            pp = new Point3D(p.x + 1, p.y, p.z);
-                            if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                dotsc.put(pp, Boolean.TRUE);
-                            }
-                            pp = new Point3D(p.x, p.y - 1, p.z);
-                            if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                dotsc.put(pp, Boolean.TRUE);
-                            }
-                            pp = new Point3D(p.x, p.y + 1, p.z);
-                            if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                dotsc.put(pp, Boolean.TRUE);
-                            }
-                            pp = new Point3D(p.x, p.y, p.z - 1);
-                            if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                dotsc.put(pp, Boolean.TRUE);
-                            }
-                            pp = new Point3D(p.x, p.y, p.z + 1);                            
-                            if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                dotsc.put(pp, Boolean.TRUE);
-                            }
-                        }
-                        int cubos = dotsc.size();
-                        dots.putAll(dotsc);
-
-                        // Sumamos 1 al número de capas con ese número de cubos
-                        C.put(cubos, !C.containsKey(cubos) ? 1 : C.get(cubos) + 1);
-                        
-                        //  Ahora calculamos nuevas capas
-/*                        int c = dots.size();
-                        while (c < 20*20*20) {
-                            HashMap<Point3D, Boolean> dotsc = new HashMap<>();
-                            Point3D pp;
-                            for (Point3D p : dots.keySet()) {
-                                pp = new Point3D(p.x - 1, p.y, p.z);
-                                if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                    dotsc.put(pp, Boolean.TRUE);
-                                }
-                                pp = new Point3D(p.x + 1, p.y, p.z);
-                                if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                    dotsc.put(pp, Boolean.TRUE);
-                                }
-                                pp = new Point3D(p.x, p.y - 1, p.z);
-                                if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                    dotsc.put(pp, Boolean.TRUE);
-                                }
-                                pp = new Point3D(p.x, p.y + 1, p.z);
-                                if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                    dotsc.put(pp, Boolean.TRUE);
-                                }
-                                pp = new Point3D(p.x, p.y, p.z - 1);
-                                if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                    dotsc.put(pp, Boolean.TRUE);
-                                }
-                                pp = new Point3D(p.x, p.y, p.z + 1);                            
-                                if (!dots.containsKey(pp) && !dotsc.containsKey(pp)) {
-                                    dotsc.put(pp, Boolean.TRUE);
-                                }
-                            }
-                            c = dotsc.size();
-                            dots.putAll(dotsc);
-                            C.put(c, !C.containsKey(c) ? 1 : C.get(c) + 1);
-                        }*/
-                    }
-                }
-            }
-            capa++;
-        }
-        for (Integer n : C.keySet()) {
-            System.out.println("C(" + n + ")=" + C.get(n));
-        }
+        // Start PE 127
         
         // End PE
 
