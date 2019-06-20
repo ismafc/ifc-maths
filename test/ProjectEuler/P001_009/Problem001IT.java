4a2f261c6783f8f31e84430e46905df44367681f
Ismael <iflores@circontrol.com>
2019-06-20 14:17:47 +0200

    Added tests. MCM, FROM and big values.

--- test/ProjectEuler/P001_009/Problem001IT.java
+++ test/ProjectEuler/P001_009/Problem001IT.java
@@ -70,6 +70,10 @@ public class Problem001IT {
         System.out.println("solution1(long A, long B, long BELOW)");
         long result = instance.solution1(A, B, BELOW);
         assertEquals(expResult, result);
+
+        // Check MCM
+        result = instance.solution1(10L, 14L, 100L);
+        assertEquals(772L, result);
     }
 
     /**
@@ -80,6 +84,10 @@ public class Problem001IT {
         System.out.println("solution1(BigInteger A, BigInteger B, BigInteger BELOW)");
         BigInteger result = instance.solution1(A_, B_, BELOW_);
         assertEquals(expResult_, result);
+
+        // Check MCM
+        result = instance.solution1(new BigInteger("10"), new BigInteger("14"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
     }
 
     /**
@@ -90,6 +98,14 @@ public class Problem001IT {
         System.out.println("solution1(long A, long B, long FROM, long BELOW)");
         long result = instance.solution1(A, B, FROM, BELOW);
         assertEquals(expResult, result);
+
+        // Check MCM
+        result = instance.solution1(10L, 14L, 1L, 100L);
+        assertEquals(772L, result);
+
+        // Check MCM and FROM
+        result = instance.solution1(10L, 14L, 50L, 100L);
+        assertEquals(588L, result);
     }
 
     /**
@@ -100,6 +116,16 @@ public class Problem001IT {
         System.out.println("solution1(BigInteger A, BigInteger B, BigInteger FROM, BigInteger BELOW)");
         BigInteger result = instance.solution1(A_, B_, FROM_, BELOW_);
         assertEquals(expResult_, result);
+
+        // Check MCM
+        result = instance.solution1(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("1"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
+
+        // Check MCM and FROM
+        result = instance.solution1(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("50"), new BigInteger("100"));
+        assertEquals(new BigInteger("588"), result);
     }
 
     /**
@@ -110,6 +136,10 @@ public class Problem001IT {
         System.out.println("solution2(long A, long B, long BELOW)");
         long result = instance.solution2(A, B, BELOW);
         assertEquals(expResult, result);
+
+        // Check MCM
+        result = instance.solution2(10L, 14L, 100L);
+        assertEquals(772L, result);
     }
 
     /**
@@ -120,6 +150,10 @@ public class Problem001IT {
         System.out.println("solution2(BigInteger A, BigInteger B, BigInteger BELOW)");
         BigInteger result = instance.solution2(A_, B_, BELOW_);
         assertEquals(expResult_, result);
+
+        // Check MCM
+        result = instance.solution2(new BigInteger("10"), new BigInteger("14"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
     }
 
     /**
@@ -130,6 +164,14 @@ public class Problem001IT {
         System.out.println("solution2(long A, long B, long FROM, long BELOW)");
         long result = instance.solution2(A, B, FROM, BELOW);
         assertEquals(expResult, result);
+
+        // Check MCM
+        result = instance.solution2(10L, 14L, 1L, 100L);
+        assertEquals(772L, result);
+
+        // Check MCM and FROM
+        result = instance.solution2(10L, 14L, 50L, 100L);
+        assertEquals(588L, result);
     }
 
     /**
@@ -140,6 +182,16 @@ public class Problem001IT {
         System.out.println("solution2(BigInteger A, BigInteger B, BigInteger FROM, BigInteger BELOW)");
         BigInteger result = instance.solution2(A_, B_, FROM_, BELOW_);
         assertEquals(expResult_, result);
+
+        // Check MCM
+        result = instance.solution2(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("1"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
+
+        // Check MCM and FROM
+        result = instance.solution2(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("50"), new BigInteger("100"));
+        assertEquals(new BigInteger("588"), result);
     }
 
     /**
@@ -150,6 +202,10 @@ public class Problem001IT {
         System.out.println("solution3(long A, long B, long BELOW)");
         long result = instance.solution3(A, B, BELOW);
         assertEquals(expResult, result);
+
+        // Check MCM
+        result = instance.solution3(10L, 14L, 100L);
+        assertEquals(772L, result);
     }
 
     /**
@@ -160,6 +216,14 @@ public class Problem001IT {
         System.out.println("solution3(long A, long B, long FROM, long BELOW)");
         long result = instance.solution3(A, B, FROM, BELOW);
         assertEquals(expResult, result);
+        
+        // Check MCM
+        result = instance.solution3(10L, 14L, 1L, 100L);
+        assertEquals(772L, result);
+
+        // Check MCM and FROM
+        result = instance.solution3(10L, 14L, 50L, 100L);
+        assertEquals(588L, result);
     }
 
     /**
@@ -170,6 +234,14 @@ public class Problem001IT {
         System.out.println("solution3(BigInteger A, BigInteger B, BigInteger BELOW)");
         BigInteger result = instance.solution3(A_, B_, BELOW_);
         assertEquals(expResult_, result);
+
+        // Check big value
+        result = instance.solution3(A_, B_, new BigInteger("10000000000"));
+        assertEquals(new BigInteger("23333333331666666668"), result);
+        
+        // Check MCM
+        result = instance.solution3(new BigInteger("10"), new BigInteger("14"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
     }
 
     /**
@@ -180,6 +252,20 @@ public class Problem001IT {
         System.out.println("solution3(BigInteger A, BigInteger B, BigInteger FROM, BigInteger BELOW)");
         BigInteger result = instance.solution3(A_, B_, FROM_, BELOW_);
         assertEquals(expResult_, result);
+
+        // Check big value and FROM
+        result = instance.solution3(A_, B_, new BigInteger("7000000000"), new BigInteger("10000000000"));
+        assertEquals(new BigInteger("11899999999500000000"), result);
+        
+        // Check MCM
+        result = instance.solution3(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("1"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
+
+        // Check MCM and FROM
+        result = instance.solution3(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("50"), new BigInteger("100"));
+        assertEquals(new BigInteger("588"), result);
     }
 
     /**
@@ -198,14 +284,9 @@ public class Problem001IT {
     @Test
     public void testSolve_int() {
         System.out.println("solve(int solution)");
-        long result = instance.solve(Problem001.Algorithm.SOLUTION1);
-        assertEquals(expResult, result);
-
-        result = instance.solve(Problem001.Algorithm.SOLUTION2);
-        assertEquals(expResult, result);
-
-        result = instance.solve(Problem001.Algorithm.SOLUTION3);
-        assertEquals(expResult, result);
+        for (Problem001.Algorithm a : Problem001.Algorithm.values()) {
+            assertEquals(expResult, instance.solve(a));
+        }
     }
 
     /**
@@ -216,6 +297,10 @@ public class Problem001IT {
         System.out.println("solve(long A, long B, long BELOW)");
         long result = instance.solve(A, B, BELOW);
         assertEquals(expResult, result);
+
+        // Check MCM
+        result = instance.solve(10L, 14L, 100L);
+        assertEquals(772L, result);
     }
 
     /**
@@ -224,14 +309,14 @@ public class Problem001IT {
     @Test
     public void testSolve_4args_1() {
         System.out.println("solve(long A, long B, long BELOW, int solution)");
-        long result = instance.solve(A, B, BELOW, Problem001.Algorithm.SOLUTION1);
-        assertEquals(expResult, result);
+        for (Problem001.Algorithm a : Problem001.Algorithm.values()) {
+            long result = instance.solve(A, B, BELOW, a);
+            assertEquals(expResult, result);
 
-        result = instance.solve(A, B, BELOW, Problem001.Algorithm.SOLUTION2);
-        assertEquals(expResult, result);
- 
-        result = instance.solve(A, B, BELOW, Problem001.Algorithm.SOLUTION3);
-        assertEquals(expResult, result);
+            // Check MCM
+            result = instance.solve(10L, 14L, 100L, a);
+            assertEquals(772L, result);
+        }
     }
 
     /**
@@ -242,6 +327,14 @@ public class Problem001IT {
         System.out.println("solve(long A, long B, long FROM, long BELOW)");
         long result = instance.solve(A, B, FROM, BELOW);
         assertEquals(expResult, result);
+
+        // Check MCM
+        result = instance.solve(10L, 14L, 1L, 100L);
+        assertEquals(772L, result);
+
+        // Check MCM and FROM
+        result = instance.solve(10L, 14L, 50L, 100L);
+        assertEquals(588L, result);
     }
 
     /**
@@ -250,14 +343,18 @@ public class Problem001IT {
     @Test
     public void testSolve_5args_1() {
         System.out.println("solve(long A, long B, long FROM, long BELOW, int solution)");
-        long result = instance.solve(A, B, FROM, BELOW, Problem001.Algorithm.SOLUTION1);
-        assertEquals(expResult, result);
+        for (Problem001.Algorithm a : Problem001.Algorithm.values()) {
+            long result = instance.solve(A, B, FROM, BELOW, a);
+            assertEquals(expResult, result);
 
-        result = instance.solve(A, B, FROM, BELOW, Problem001.Algorithm.SOLUTION2);
-        assertEquals(expResult, result);
+            // Check MCM
+            result = instance.solve(10L, 14L, 1L, 100L, a);
+            assertEquals(772L, result);
 
-        result = instance.solve(A, B, FROM, BELOW, Problem001.Algorithm.SOLUTION3);
-        assertEquals(expResult, result);
+            // Check MCM and FROM
+            result = instance.solve(10L, 14L, 50L, 100L, a);
+            assertEquals(588L, result);
+        }
     }
 
     /**
@@ -268,6 +365,14 @@ public class Problem001IT {
         System.out.println("solve(BigInteger A, BigInteger B, BigInteger BELOW)");
         BigInteger result = instance.solve(A_, B_, BELOW_);
         assertEquals(expResult_, result);
+        
+        // Check big value
+        result = instance.solve(A_, B_, new BigInteger("10000000000"));
+        assertEquals(new BigInteger("23333333331666666668"), result);
+
+        // Check MCM
+        result = instance.solve(new BigInteger("10"), new BigInteger("14"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
     }
 
     /**
@@ -276,14 +381,21 @@ public class Problem001IT {
     @Test
     public void testSolve_4args_3() {
         System.out.println("solve(BigInteger A, BigInteger B, BigInteger BELOW, int solution)");
-        BigInteger result = instance.solve(A_, B_, BELOW_, Problem001.Algorithm.SOLUTION1);
-        assertEquals(expResult_, result);
+        for (Problem001.Algorithm a : Problem001.Algorithm.values()) {
+            BigInteger result = instance.solve(A_, B_, BELOW_, a);
+            assertEquals(expResult_, result);
 
-        result = instance.solve(A_, B_, BELOW_, Problem001.Algorithm.SOLUTION2);
-        assertEquals(expResult_, result);
-
-        result = instance.solve(A_, B_, BELOW_, Problem001.Algorithm.SOLUTION3);
-        assertEquals(expResult_, result);
+            if (a == Problem001.Algorithm.SOLUTION3) {
+                // Check big value
+                result = instance.solve(A_, B_, new BigInteger("10000000000"), a);
+                assertEquals(new BigInteger("23333333331666666668"), result);
+            }
+            
+            // Check MCM
+            result = instance.solve(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("100"), a);
+            assertEquals(new BigInteger("772"), result);
+        }
     }
 
     /**
@@ -294,6 +406,20 @@ public class Problem001IT {
         System.out.println("solve(BigInteger A, BigInteger B, BigInteger FROM, BigInteger BELOW)");
         BigInteger result = instance.solve(A_, B_, FROM_, BELOW_);
         assertEquals(expResult_, result);
+        
+        // Check big value and FROM
+        result = instance.solve(A_, B_, new BigInteger("7000000000"), new BigInteger("10000000000"));
+        assertEquals(new BigInteger("11899999999500000000"), result);
+
+        // Check MCM
+        result = instance.solve(new BigInteger("10"), new BigInteger("14"), 
+                                new BigInteger("1"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
+        
+        // Check MCM and FROM
+        result = instance.solve(new BigInteger("10"), new BigInteger("14"), 
+                                new BigInteger("50"), new BigInteger("100"));
+        assertEquals(new BigInteger("588"), result);
     }
 
     /**
@@ -302,14 +428,26 @@ public class Problem001IT {
     @Test
     public void testSolve_5args_2() {
         System.out.println("solve(BigInteger A, BigInteger B, BigInteger FROM, BigInteger BELOW, int solution)");
-        BigInteger result = instance.solve(A_, B_, FROM_, BELOW_, Problem001.Algorithm.SOLUTION1);
-        assertEquals(expResult_, result);
+        for (Problem001.Algorithm a : Problem001.Algorithm.values()) {
+            BigInteger result = instance.solve(A_, B_, FROM_, BELOW_, a);
+            assertEquals(expResult_, result);
+            
+            if (a == Problem001.Algorithm.SOLUTION3) {
+                // Check big value and FROM
+                result = instance.solve(A_, B_, new BigInteger("7000000000"), new BigInteger("10000000000"), a);
+                assertEquals(new BigInteger("11899999999500000000"), result);
+            }
+            
+            // Check MCM
+            result = instance.solve(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("1"), new BigInteger("100"), a);
+            assertEquals(new BigInteger("772"), result);
 
-        result = instance.solve(A_, B_, FROM_, BELOW_, Problem001.Algorithm.SOLUTION2);
-        assertEquals(expResult_, result);
-
-        result = instance.solve(A_, B_, FROM_, BELOW_, Problem001.Algorithm.SOLUTION3);
-        assertEquals(expResult_, result);
+            // Check MCM and FROM
+            result = instance.solve(new BigInteger("10"), new BigInteger("14"), 
+                                    new BigInteger("50"), new BigInteger("100"), a);
+            assertEquals(new BigInteger("588"), result);
+        }
     }
 
     /**
@@ -321,6 +459,15 @@ public class Problem001IT {
         ArrayList<Long> V = new ArrayList<>(Arrays.asList(A, B));
         long result = instance.solve(V, FROM, BELOW);
         assertEquals(expResult, result);
+
+        // Check MCM
+        V = new ArrayList<>(Arrays.asList(10L, 14L));
+        result = instance.solve(V, 1L, 100L);
+        assertEquals(772L, result);
+
+        // Check MCM and FROM
+        result = instance.solve(V, 50L, 100L);
+        assertEquals(588L, result);
     }
 
     /**
@@ -329,15 +476,20 @@ public class Problem001IT {
     @Test
     public void testSolve_4args_5() {
         System.out.println("solve(ArrayList<Long> V, long FROM, long BELOW, int solution)");
-        ArrayList<Long> V = new ArrayList<>(Arrays.asList(A, B));
-        long result = instance.solve(V, FROM, BELOW, Problem001.Algorithm.SOLUTION1);
-        assertEquals(expResult, result);
-        
-        result = instance.solve(V, FROM, BELOW, Problem001.Algorithm.SOLUTION2);
-        assertEquals(expResult, result);
-
-        result = instance.solve(V, FROM, BELOW, Problem001.Algorithm.SOLUTION3);
-        assertEquals(expResult, result);
+        for (Problem001.Algorithm a : Problem001.Algorithm.values()) {
+            ArrayList<Long> V = new ArrayList<>(Arrays.asList(A, B));
+            long result = instance.solve(V, FROM, BELOW, a);
+            assertEquals(expResult, result);
+            
+            // Check MCM
+            V = new ArrayList<>(Arrays.asList(10L, 14L));
+            result = instance.solve(V, 1L, 100L, a);
+            assertEquals(772L, result);
+            
+            // Check MCM and FROM
+            result = instance.solve(V, 50L, 100L, a);
+            assertEquals(588L, result);
+        }        
     }
 
     /**
@@ -349,6 +501,19 @@ public class Problem001IT {
         ArrayList<BigInteger> V = new ArrayList<>(Arrays.asList(A_, B_));
         BigInteger result = instance.solve(V, FROM_, BELOW_);
         assertEquals(expResult_, result);
+        
+        // Check big value and FROM
+        result = instance.solve(V, new BigInteger("7000000000"), new BigInteger("10000000000"));
+        assertEquals(new BigInteger("11899999999500000000"), result);
+                
+        // Check MCM
+        V = new ArrayList<>(Arrays.asList(new BigInteger("10"), new BigInteger("14")));
+        result = instance.solve(V, new BigInteger("1"), new BigInteger("100"));
+        assertEquals(new BigInteger("772"), result);
+        
+        // Check MCM and FROM
+        result = instance.solve(V, new BigInteger("50"), new BigInteger("100"));
+        assertEquals(new BigInteger("588"), result);
     }
 
     /**
@@ -357,15 +522,26 @@ public class Problem001IT {
     @Test
     public void testSolve_4args_6() {
         System.out.println("solve(ArrayList<BigInteger> V, BigInteger FROM, BigInteger BELOW, int solution)");
-        ArrayList<BigInteger> V = new ArrayList<>(Arrays.asList(A_, B_));
-        BigInteger result = instance.solve(V, FROM_, BELOW_, Problem001.Algorithm.SOLUTION1);
-        assertEquals(expResult_, result);
+        for (Problem001.Algorithm a : Problem001.Algorithm.values()) {
+            ArrayList<BigInteger> V = new ArrayList<>(Arrays.asList(A_, B_));
+            BigInteger result = instance.solve(V, FROM_, BELOW_, a);
+            assertEquals(expResult_, result);
         
-        result = instance.solve(V, FROM_, BELOW_, Problem001.Algorithm.SOLUTION2);
-        assertEquals(expResult_, result);
-        
-        result = instance.solve(V, FROM_, BELOW_, Problem001.Algorithm.SOLUTION3);
-        assertEquals(expResult_, result);
+            if (a == Problem001.Algorithm.SOLUTION3) {
+                // Check big value and FROM
+                result = instance.solve(V, new BigInteger("7000000000"), new BigInteger("10000000000"), a);
+                assertEquals(new BigInteger("11899999999500000000"), result);
+            }
+            
+            // Check MCM
+            V = new ArrayList<>(Arrays.asList(new BigInteger("10"), new BigInteger("14")));
+            result = instance.solve(V, new BigInteger("1"), new BigInteger("100"), a);
+            assertEquals(new BigInteger("772"), result);
+            
+            // Check MCM and FROM
+            result = instance.solve(V, new BigInteger("50"), new BigInteger("100"), a);
+            assertEquals(new BigInteger("588"), result);
+        }
     }
     
 }
