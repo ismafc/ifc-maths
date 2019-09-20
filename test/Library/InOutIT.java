312d543b5fd36d88f5d454cbfa3a81a2e522bed1
Ismael <iflores@circontrol.com>
2019-09-20 09:35:35 +0200

    Test for function getDuration

--- test/Library/InOutIT.java
+++ test/Library/InOutIT.java
@@ -6,6 +6,8 @@
 package Library;
 
 import java.math.BigInteger;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -124,6 +126,77 @@ public class InOutIT {
         name = "BIG";
         expResult = new BigInteger("18362781233492778327891");
         result = InOut.getArgument(args, default_value, name);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getDuration method, of class InOut.
+     */
+    @Test
+    public void testGetDuration() {
+        System.out.println("getDuration(long millis, ResourceBundle bundle)");
+        Locale.setDefault(Locale.ENGLISH);
+        ResourceBundle bundle = ResourceBundle.getBundle("resources/messages");
+        long millis = 0L;
+        String expResult = "0 Milliseconds";
+        String result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+
+        millis = 2L * 1000L + 1L;
+        expResult = "2 Seconds, 1 Millisecond";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+        
+        millis = 1L * 60L * 1000L + 5L * 1000L + 12L;
+        expResult = "1 Minute, 5 Seconds, 12 Milliseconds";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+
+        millis = 1L * 3600L * 1000L + 15L * 60L * 1000L;
+        expResult = "1 Hour, 15 Minutes, 0 Seconds, 0 Milliseconds";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+        
+        millis = 28L * 3600L * 1000L + 3L * 60L * 1000L + 1L * 1000L + 123L;
+        expResult = "1 Day, 4 Hours, 3 Minutes, 1 Second, 123 Milliseconds";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+        
+        millis = 48L * 3600L * 1000L + 12L * 1000L + 1L;
+        expResult = "2 Days, 0 Hours, 0 Minutes, 12 Seconds, 1 Millisecond";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+        
+        Locale.setDefault(new Locale("es", "ES"));
+        bundle = ResourceBundle.getBundle("resources/messages");
+        millis = 0L;
+        expResult = "0 Milisegundos";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+
+        millis = 2L * 1000L + 1L;
+        expResult = "2 Segundos, 1 Milisegundo";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+        
+        millis = 1L * 60L * 1000L + 5L * 1000L + 12L;
+        expResult = "1 Minuto, 5 Segundos, 12 Milisegundos";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+
+        millis = 1L * 3600L * 1000L + 15L * 60L * 1000L;
+        expResult = "1 Hora, 15 Minutos, 0 Segundos, 0 Milisegundos";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+        
+        millis = 28L * 3600L * 1000L + 3L * 60L * 1000L + 1L * 1000L + 123L;
+        expResult = "1 Día, 4 Horas, 3 Minutos, 1 Segundo, 123 Milisegundos";
+        result = InOut.getDuration(millis, bundle);
+        assertEquals(expResult, result);
+        
+        millis = 48L * 3600L * 1000L + 12L * 1000L + 1L;
+        expResult = "2 Días, 0 Horas, 0 Minutos, 12 Segundos, 1 Milisegundo";
+        result = InOut.getDuration(millis, bundle);
         assertEquals(expResult, result);
     }
     
